8ba1ffd240dcb4e1f3ae50676245b96b569d3565
Vedran Subotic <vedran.subotic@gmail.com>
2013-01-24 02:08:39 +0100

    code cleanup

--- app/code/community/Jarlssen/HolePunch/Block/Catalog/Product/View.php
+++ app/code/community/Jarlssen/HolePunch/Block/Catalog/Product/View.php
@@ -6,8 +6,6 @@ class Jarlssen_HolePunch_Block_Catalog_Product_View extends Mage_Catalog_Block_P
     protected function _construct()
     {
         $this->addData(array(
-        	//'cache_key' 		=>	
-            //'cache_lifetime'  => 
             'cache_tags'        => array(Mage_Catalog_Model_Product::CACHE_TAG . "_" . $this->getProduct()->getId()),
         ));
     }
@@ -15,19 +13,18 @@ class Jarlssen_HolePunch_Block_Catalog_Product_View extends Mage_Catalog_Block_P
     public function getCacheKey()
     {
         if (!$this->hasData('cache_key')) {
-       		//$cacheKey = LAYOUTNAME_STORE+ID_PRODUCT+ID
-        	$cacheKey = $this->getNameInLayout().'_STORE'.Mage::app()->getStore()->getId().'_PRODUCT'.$this->getProduct()->getId();
-        	//.'_'.Mage::getDesign()->getPackageName().'_'.Mage::getDesign()->getTheme('template'). //_PACKAGE_THEME ?
+        	$cacheKey = $this->getNameInLayout().'_STORE_'.Mage::app()->getStore()->getId().'_PRODUCT_'.$this->getProduct()->getId();
         	$this->setCacheKey($cacheKey);
         }
         return $this->getData('cache_key');
     }
     
     public function getCacheLifetime()
-    {	  //to prevent sub-blocks caching
-    	  if($this->getNameInLayout()!='product.info') return null;
-    	  //return false; //false creates default lifetime (7200)
-    	  return 9999999999;
+    {
+    	  if($this->getNameInLayout() != 'product.info'){
+              return null;
+          }
+    	  return 300;
     }
     
 }
