df380305cd41713f3d1f1fa4a8c4f87611aed432
Vedran Subotic <vedran.subotic@gmail.com>
2013-02-19 01:54:54 +0100

    Rewrites Mage_Catalog_Block_Product_List and adds child block output.

---
+++ app/code/community/Jarlssen/HolePunch/Block/Catalog/Product/List.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Jarlssen_HolePunch_Block_Catalog_Product_List
+ *
+ * Rewrites Mage_Catalog_Block_Product_List and adds child block output.
+ *
+ * License: GNU General Public License
+ *
+ * Copyright (c) 2012 Jarlssen GmbH. All rights reserved.
+ * Note: Original work copyright to respective authors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category   Jarlssen
+ * @package    Jarlssen_HolePunch
+ * @subpackage Model
+ * @copyright  Copyright (c) 1996-2012 Jarlssen GmbH (http://www.Jarlssen.com)
+ * @license	   http://www.gnu.org/licenses/gpl.html GPL, version 3
+ * @version    $Id:$
+ * @link       http://www.Jarlssen.com
+ * @since      File available since Release 0.1.0
+ * @author     Jarlssen Core Team <core@Jarlssen.com>
+ */
+
+/**
+ * Jarlssen_HolePunch_Block_Catalog_Product_List
+ *
+ * Rewrites Mage_Catalog_Block_Product_List and adds child block output.
+ *
+ * @category   Jarlssen
+ * @package    Jarlssen_HolePunch
+ * @subpackage Model
+ * @copyright  Copyright (c) 1996-2012 Jarlssen GmbH (http://www.Jarlssen.com)
+ * @license    http://www.gnu.org/licenses/gpl.html GPL, version 3
+ * @version    Release: @package_version@
+ * @since      Class available since Release 0.1.0
+ * @author     Jarlssen Core Team <core@Jarlssen.com>
+ */
+class Jarlssen_HolePunch_Block_Catalog_Product_List extends Mage_Catalog_Block_Product_List
+{
+
+    /*
+     *  class of the child block called in catalog/product/view.phtml template
+     */
+    protected $_defaultItemPriceBlock = 'jarlssen_holepunch/catalog_product_list_item_price';
+
+    /*
+     *  name in layout that is reference name for hole puncher
+     */
+    protected $_defaultItemPriceBlockName = 'catalog.product.list.item.price';
+
+
+    /*
+     *
+     *  @return Jarlssen_HolePunch_Block_Catalog_Product_List_Item_Price
+     */
+    public function getItemPriceBlock()
+    {
+        $block = $this->getLayout()->createBlock($this->_defaultItemPriceBlock, $this->_defaultItemPriceBlockName);
+
+        return $block;
+    }
+}
